598d1c0df302124d0bef865d04836832e6b69bc2
Dmitry Matveyev <dmitry.matveyev.job@gmail.com>
2018-11-16 17:36:07 +0300

    Rename lables and tune disrtibution options

--- Scatterbrain/Scatterbrain/Behaviors/DepartmentsBehavior.cs
+++ Scatterbrain/Scatterbrain/Behaviors/DepartmentsBehavior.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
-using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -12,34 +11,31 @@ using Xamarin.Forms;
 
 namespace Scatterbrain.Behaviors
 {
-    public class SubjectsBehavior : SfListViewBehavior
+    public class DepartmentsBehavior : SfListViewBehavior
     {
         public SfListView Departments { get; set; }
-        public SfListView Subjects { get; set; }
 
         protected override void OnAttachedTo(SfListView lv)
         {
             base.OnAttachedTo(lv);
-            Subjects = lv;
-            Subjects.Loaded += ObserveSubjectsLoaded;
+            Departments = lv;
+            Departments.Loaded += ObserveDepartmentsLoaded;
         }
 
         protected override void OnDetachingFrom(SfListView lv)
         {
             base.OnDetachingFrom(lv);
-            Subjects.Loaded -= ObserveSubjectsLoaded;
+            Departments.Loaded -= ObserveDepartmentsLoaded;
         }
 
-        private void ObserveSubjectsLoaded(object sender, ListViewLoadedEventArgs e)
+        private void ObserveDepartmentsLoaded(object sender, ListViewLoadedEventArgs e)
         {
-            var itemsSorce = ((SfListView)sender).ItemsSource as ObservableCollection<Subject>;
+            var itemsSorce = ((SfListView)sender).ItemsSource as ObservableCollection<Department>;
             itemsSorce.CollectionChanged += ObserveItemsSource;
-            ObserveItemsSource(null, null);
         }
 
         private void ObserveItemsSource(object sender, NotifyCollectionChangedEventArgs e)
         {
-            UpdateListView(Subjects);
             UpdateListView(Departments);
         }
     }
---
+++ Scatterbrain/Scatterbrain/Behaviors/SfListViewBehavior.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Scatterbrain.Behaviors
+{
+    public class SfListViewBehavior : Behavior<SfListView>
+    {
+        public static void UpdateListView(object sender)
+        {
+            if (!(sender is SfListView listView))
+            {
+                return;
+            }
+
+            var visualContainer = listView.GetType().GetRuntimeProperties().First(p => p.Name == "VisualContainer").GetValue(listView) as VisualContainer;
+
+            var totalExtent = (double)visualContainer.GetType().GetRuntimeProperties().FirstOrDefault(container => container.Name == "TotalExtent").GetValue(visualContainer);
+
+            listView.HeightRequest = totalExtent;
+
+            listView.RefreshView();
+            listView.ForceLayout();
+        }
+    }
+}
